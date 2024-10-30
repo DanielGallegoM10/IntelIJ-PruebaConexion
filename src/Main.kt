f62f88ef4f9023ec1fc959591392d675e989e3ba
DanielG <danielgallegom10@gmail.com>
2024-10-30 12:23:29 +0100

    Creation and using fun helloworld

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
     println("Este proyecto esta hecho con Kotlin y servira para conectar con github")
+    helloworld("Daniel")
+}
+fun helloworld(name: String){
+    println("Helo $name")
 }
